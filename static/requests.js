014c70efa0ff9b87872a31a72c3516f1fb9a9e86
AlexK <alexkizia@gmail.com>
2020-01-19 03:11:39 +0000

    Added all 6 plots. Now need to make fully dynamic, interactive, and communicative

--- static/requests.js
+++ static/requests.js
@@ -1,24 +1,48 @@
 function start_up(val){
     TESTER = document.getElementById('tester');
     console.log(val.previous.TV);
+    var x_a = [...Array(168).keys()];
+    var x_b = [...Array(168).keys()];
+    for (var i=0; i<168; i++){
+        x_b[i] = 168+i; 
+    }
     var trace = {
-        x: [...Array(336).keys()],
+        x: x_a,
         y: val.previous.fridge,
         name: "Fridge (history)"
     };
     var trace2 = {
-        x: [...Array(336).keys()],
+        x: x_a,
         y: val.previous.heating,
         name: "Heating (history)"
     };
     var trace3 = {
-        x: [...Array(336).keys()],
+        x: x_a,
         y: val.previous.TV ,
         name: "Television (history)"
     };
     var fridge_previous = [trace];
     var heating_previous = [trace2];
     var TV_previous = [trace3];
+    
+    var trace = {
+        x: x_b,
+        y: val.future.fridge,
+        name: "Fridge (future)"
+    };
+    var trace2 = {
+        x: x_b,
+        y: val.future.heating,
+        name: "Heating (future)"
+    };
+    var trace3 = {
+        x: x_b,
+        y: val.future.TV ,
+        name: "Television (future)"
+    };
+    var fridge_future = [trace];
+    var heating_future = [trace2];
+    var TV_future = [trace3];
 
     var layout = {
         font: {
@@ -58,6 +82,10 @@ function start_up(val){
     Plotly.plot( TESTER, fridge_previous, layout, {responsive: true} );
     Plotly.plot( TESTER, heating_previous, layout, {responsive: true} );
     Plotly.plot( TESTER, TV_previous, layout, {responsive: true} );
+
+    Plotly.plot( TESTER, fridge_future, layout, {responsive: true} );
+    Plotly.plot( TESTER, heating_future, layout, {responsive: true} );
+    Plotly.plot( TESTER, TV_future, layout, {responsive: true} );
 }
 
 
